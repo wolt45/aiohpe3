e175056b533567ec87821e15bf87c50ba977c732
Sophrosyne <majanimsaj@yahoo.com>
2014-09-17 12:40:53 +0800

    hpdemo
    hpdemo

--- www/myservices/model.js
+++
@@ -1,8 +0,0 @@
-$data.EntityContext.extend("$ipadrbg.types.ngpxContext", {
-	clinix: { type: $data.EntitySet, elementType: $ipadrbg.types.clinix }
-	, clinix_chiefcomp: { type: $data.EntitySet, elementType: $ipadrbg.types.clinix_chiefcomp }
-	, clinix_spineIntl: { type: $data.EntitySet, elementType: $ipadrbg.types.clinix_spineIntl }
-	, clinix_etiology: { type: $data.EntitySet, elementType: $ipadrbg.types.clinix_etiology }
-});
-
-$ipadrbg.context = new $ipadrbg.types.ngpxContext({ name: "webSql", databaseName: "ipadrbg" });
