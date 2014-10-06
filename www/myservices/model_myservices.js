2840b00d9784cc82ae83aa1cb9cc6e713503f4df
Sophrosyne <majanimsaj@yahoo.com>
2014-10-06 09:28:47 +0800

    delete
    delete

--- www/myservices/model_myservices.js
+++
@@ -1,5 +0,0 @@
-$data.EntityContext.extend("$ipadrbg.types.ngpxContext", {
-	clinix: { type: $data.EntitySet, elementType: $ipadrbg.types.clinix }
-});
-
-$ipadrbg.context = new $ipadrbg.types.ngpxContext({ name: "webSql", databaseName: "ipadrbg" });
