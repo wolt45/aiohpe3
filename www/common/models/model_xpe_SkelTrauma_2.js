64746ed6b205769c097d34cfdbc2e40effe12b02
Sophrosyne <majanimsaj@yahoo.com>
2015-10-22 14:51:08 +0800

    QQQ
    QQQ

--- www/common/models/model_xpe_SkelTrauma_2.js
+++ www/common/models/model_xpe_SkelTrauma_2.js
@@ -1,27 +1,27 @@
 $data.Entity.extend("$ipadrbg.types.jdata_SkelTrauma_2", {
-	SkelTraumaRID: { type: "int", key: true, computed: true }
-	, ClinixRID: { type: "string"}
-	, PxRID: { type: "string"}
+	SkelTraumaRID	: { type: "int", key: true, computed: true }
+	, ClinixRID		: { type: "string"}
+	, PxRID			: { type: "string"}
 	
-	, ApproXraysOrderedSpine: { type: "string"}
+	, ApproXraysOrderedSpine		: { type: "string"}
 	
-	, ApproXraysOrderedHumerus: { type: "string"}
-	, ApproXraysOrderedRadiusUlna: { type: "string"}
-	, ApproXraysOrderedShoulder: { type: "string"}
-	, ApproXraysOrderedElbows: { type: "string"}
-	, ApproXraysOrderedWrist: { type: "string"}
-	, ApproXraysOrderedHand: { type: "string"}
-	, ApproXraysOrderedPelvis: { type: "string"}
-	, ApproXraysOrderedHip: { type: "string"}
-	, ApproXraysOrderedKnee: { type: "string"}
-	, ApproXraysOrderedTibia: { type: "string"}
-	, ApproXraysOrderedAnkle: { type: "string"}
-	, ApproXraysOrderedFoot: { type: "string"}
-	, XrayFindDate: { type: "string"}
-	, NormalAbnormal: { type: "string"}
-	, Fracture: { type: "string"}
-	, SiteShaft: { type: "string"}
-	, Dislocation: { type: "string"}
+	, ApproXraysOrderedHumerus		: { type: "string"}
+	, ApproXraysOrderedRadiusUlna	: { type: "string"}
+	, ApproXraysOrderedShoulder		: { type: "string"}
+	, ApproXraysOrderedElbows		: { type: "string"}
+	, ApproXraysOrderedWrist		: { type: "string"}
+	, ApproXraysOrderedHand			: { type: "string"}
+	, ApproXraysOrderedPelvis		: { type: "string"}
+	, ApproXraysOrderedHip			: { type: "string"}
+	, ApproXraysOrderedKnee			: { type: "string"}
+	, ApproXraysOrderedTibia		: { type: "string"}
+	, ApproXraysOrderedAnkle		: { type: "string"}
+	, ApproXraysOrderedFoot			: { type: "string"}
+	, XrayFindDate					: { type: "string"}
+	, NormalAbnormal				: { type: "string"}
+	, Fracture						: { type: "string"}
+	, SiteShaft						: { type: "string"}
+	, Dislocation					: { type: "string"}
 	
-	, SynchStatus: { type: "string"}
+	, SynchStatus					: { type: "string"}
 });
--- www/common/models/model_xpe_skeltrauma_1.js
+++ www/common/models/model_xpe_skeltrauma_1.js
@@ -1,16 +1,16 @@
 $data.Entity.extend("$ipadrbg.types.jdata_SkelTrauma_1", {
 	SkelTraumaRID: { type: "int", key: true, computed: true }
-	, ClinixRID: { type: "string"}
-	, PxRID: { type: "string"}
+	, ClinixRID				: { type: "string"}
+	, PxRID					: { type: "string"}
 	
-	, AppearanceNormal: { type: "string"}
-	, Deformity: { type: "string"}
-	, Skin: { type: "string"}
-	, PainActiveMotion: { type: "string"}
-	, PainPassiveMotion: { type: "string"}
-	, NeurologicStatus: { type: "string"}
-	, VascularStatus: { type: "string"}
-	, NeurologicVascularDes: { type: "string"}
+	, AppearanceNormal		: { type: "string"}
+	, Deformity				: { type: "string"}
+	, Skin					: { type: "string"}
+	, PainActiveMotion		: { type: "string"}
+	, PainPassiveMotion		: { type: "string"}
+	, NeurologicStatus		: { type: "string"}
+	, VascularStatus		: { type: "string"}
+	, NeurologicVascularDes	: { type: "string"}
 	
-	, SynchStatus: { type: "string"}
+	, SynchStatus			: { type: "string"}
 });
