9be646a8b5880ad9291315d0e10e1ca8a42fe715
Sophrosyne <majanimsaj@yahoo.com>
2015-10-27 15:17:08 +0800

    Q
    Q

--- www/pe/js/load_models_pe.js
+++ www/pe/js/load_models_pe.js
@@ -104,7 +104,6 @@ $data.EntityContext.extend("$ipadrbg.types.ngpxContext", {
 	, jdata_shoulder_9: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_shoulder_9 }
 
 	, jdata_SkelTrauma_1: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_SkelTrauma_1 }
-	, jdata_SkelTrauma_2: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_SkelTrauma_2 }
 
 });
 
