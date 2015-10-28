9805d851c953027c6a0fdfb64f01672028bb5f19
Sophrosyne <majanimsaj@yahoo.com>
2015-10-28 12:45:22 +0800

    Q
    Q

--- www/myservices/model_myservices.js
+++ www/myservices/model_myservices.js
@@ -57,6 +57,51 @@ $data.EntityContext.extend("$ipadrbg.types.ngpxContext", {
 	, clinix_StructuredMedication: { type: $data.EntitySet, elementType: $ipadrbg.types.clinix_StructuredMedication }
 	
 	, zclinix: { type: $data.EntitySet, elementType: $ipadrbg.types.zclinix }
+
+
+ 	, jdata_SkelTrauma_1: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_SkelTrauma_1 }
+ 	, jdata_SkelTrauma_2: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_SkelTrauma_2 }
+	
+	, jdata_genotho_1: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_genotho_1 }
+	, jdata_genotho_2: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_genotho_2 }
+	, jdata_genotho_3: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_genotho_3 }
+	, jdata_genotho_4: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_genotho_4 }
+	, jdata_genotho_5: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_genotho_5 }
+	, jdata_genotho_6: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_genotho_6 }
+
+	, jdata_spine_1: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_spine_1 }
+	, jdata_spine_2: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_spine_2 }
+	, jdata_spine_3: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_spine_3 }
+	, jdata_spine_4: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_spine_4 }
+
+	, jdata_FootAnkle_1: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_FootAnkle_1 }
+	, jdata_FootAnkle_2: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_FootAnkle_2 }
+	, jdata_FootAnkle_3: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_FootAnkle_3 }
+	, jdata_FootAnkle_4: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_FootAnkle_4 }
+	, jdata_FootAnkle_5: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_FootAnkle_5 }
+	, jdata_FootAnkle_6: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_FootAnkle_6 }
+	, jdata_FootAnkle_7: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_FootAnkle_7 }
+	, jdata_FootAnkle_8: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_FootAnkle_8 }
+	, jdata_FootAnkle_9: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_FootAnkle_9 }
+
+	, jdata_Knee_1: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_Knee_1 }
+	, jdata_Knee_2: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_Knee_2 }
+	, jdata_Knee_3: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_Knee_3 }
+	, jdata_Knee_4: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_Knee_4 }
+	, jdata_Knee_5: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_Knee_5 }
+	, jdata_Knee_6: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_Knee_6 }
+	, jdata_Knee_7: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_Knee_7 }
+	, jdata_Knee_8: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_Knee_8 }
+
+	, jdata_shoulder_1: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_shoulder_1}
+	, jdata_shoulder_2: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_shoulder_2}
+	, jdata_shoulder_3: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_shoulder_3}
+	, jdata_shoulder_4: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_shoulder_4}
+	, jdata_shoulder_5: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_shoulder_5}
+	, jdata_shoulder_6: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_shoulder_6}
+	, jdata_shoulder_7: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_shoulder_7}
+	, jdata_shoulder_8: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_shoulder_8}
+	, jdata_shoulder_9: { type: $data.EntitySet, elementType: $ipadrbg.types.jdata_shoulder_9}
 });
 
 $ipadrbg.context = new $ipadrbg.types.ngpxContext({ name: "webSql", databaseName: "ipadrbg" });
